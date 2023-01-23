c3e04c5b7534abea9a08cc33bda8c1006e49a090
PrinceCharles47 <clemente.pcharles@gmail.com>
2023-01-23 19:30:33 +0700

    revamp design

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card
+  <!-- <v-card
   max-width="500"
   dark
   class="mx-auto mt-10 pa-5"
@@ -44,11 +44,11 @@
       v-for="response in apiResponse" :key="response.anilist.id"
       >
 
-        <!-- <v-img
+        <v-img
         :src="response.image"
         max-width="175"
         class="mr-2 rounded-sm"
-        ></v-img> -->
+        ></v-img>
 
         <video class="video" :src="response.video"></video>
 
@@ -67,7 +67,87 @@
       </v-card>
     </v-container>
 
-  </v-card>
+  </v-card> -->
+
+  <v-container>
+
+    <v-row
+    class="d-flex justify-center my-5"
+    >
+
+      <v-card
+      id="title"
+      dark
+      >
+
+        <v-card-title
+        class="d-flex flex-column align-center"
+        >
+          ASK FOR SAUCE NO MORE!
+        </v-card-title>
+        
+        <v-card-text>
+          Upload the image of the anime scene to know  the title of the show and other details.
+        </v-card-text>
+
+        <v-file-input
+        label="Input image file"
+        prepend-icon="mdi-image"
+        v-model="imageUpload"
+        outlined
+        dense
+        >
+        </v-file-input>
+
+        <v-btn
+        block
+        outlined
+        class="white black--text"
+        @click="onChange"
+        >
+          <v-icon>mdi-upload</v-icon>
+          Upload Image
+        </v-btn>
+
+      </v-card>
+    
+    </v-row>
+
+    <v-row
+    v-if="apiResponse"
+    class="justify-space-between align-center"
+    >
+
+      <v-card
+      width="350"
+      class="results-card mb-5"
+      dark
+      v-for="response in apiResponse"
+      :key="response.anilist.id"
+      >
+
+        <v-img
+        :src="response.image"
+        contain
+        >
+        </v-img>
+
+        <v-card-title>
+          {{ response.anilist.title.romaji }}
+        </v-card-title>
+
+        <!-- <v-spacer></v-spacer> -->
+
+        <v-card-text>
+          Episode {{ response.episode }}
+        </v-card-text>
+
+      </v-card>
+
+    </v-row>
+
+  </v-container>
+
 </template>
 
 <script>
@@ -103,14 +183,25 @@
   border: 1px solid grey;
 } */
 
-#card{
+/* #card{
   background-color: transparent;
   box-shadow: none;
-  /* border: 1px solid white; */
+  border: 1px solid white;
 }
 
 .video{
   max-width: 175px;
+} */
+
+#title{
+  background-color: transparent;
+  box-shadow: none;
+}
+
+.results-card{
+  background-color: transparent;
+  box-shadow: none;
+  border: 1px solid gray;
 }
 
 </style>
